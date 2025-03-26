58e8e71a3c8398c3c69f76d3952bcd7ded8ce26f
admin <admin@DESKTOP-8NNH5PG>
2025-03-26 17:27:54 +0900

    3월26일

--- Project/src/Coffee/CoffeeApp.java
+++ Project/src/Coffee/CoffeeApp.java
@@ -18,6 +18,7 @@ public class CoffeeApp { // 메인 콘솔 앱 클래스
         System.out.println("1. 매장 주문");
         System.out.println("2. 포장 주문");
         System.out.println("9. 종료");
+        System.out.println("====================");
         System.out.print("선택>> ");
         int orderType = scanner.nextInt();
         scanner.nextLine(); // 버퍼 비우기	
@@ -37,7 +38,7 @@ public class CoffeeApp { // 메인 콘솔 앱 클래스
         		List<String> categories = dao.getCategories(); // DB에서 카테고리 목록 조회
             
         		// 메인 메뉴 출력
-        		System.out.println("==== [메뉴 선택] ====");
+        		System.out.println("===== [메뉴 선택] =====");
 	            for (int i = 0; i < categories.size(); i++) {
 	                System.out.printf("%d. %s\n", i + 1, categories.get(i));
 	            }
@@ -46,8 +47,8 @@ public class CoffeeApp { // 메인 콘솔 앱 클래스
 	            System.out.println(cartMenu + ". 장바구니 확인/수정");
 	            System.out.println(payMenu + ". 결제");
 	            System.out.println("9. 종료");
+	            System.out.println("====================");
 	            System.out.print("선택>> ");
-	
 	            int menu = 9; // 기본값은 종료
 	            while (true) {
 	                try {
@@ -133,13 +134,14 @@ public class CoffeeApp { // 메인 콘솔 앱 클래스
         }
 
         while (true) {
-            System.out.println("===== 장바구니 =====");
+            System.out.println("============ [장바구니] ============");
             for (int i = 0; i < cart.size(); i++) {
                 Coffee item = cart.get(i);
-                System.out.printf("%d. %s - %s x %d = %d원\n",
+                System.out.printf("%d. %s : %s x %d = %d원\n",
                     i + 1, item.getCategory(), item.getName(),
                     item.getQuantity(), item.getTotalPrice()); // 항목 출력
             }
+            System.out.println("=================================");
             System.out.println("0. 수량 수정 / 삭제");
             System.out.println("1. 결제 진행");
             System.out.println("2. 이전 화면으로");
@@ -187,14 +189,14 @@ public class CoffeeApp { // 메인 콘솔 앱 클래스
         }
 
         int total = 0;
-        System.out.println("===== 주문 내역 =====");
+        System.out.println("============ [주문 내역] ============");
         for (Coffee item : cart) {
             System.out.printf("%s - %s x %d = %d원\n",
                 item.getCategory(), item.getName(),
                 item.getQuantity(), item.getTotalPrice()); // 항목 출력
             total += item.getTotalPrice();
         }
-
+        System.out.println("==================================");
         System.out.println("총 결제 금액: " + total + "원");
         System.out.print("지불하실 금액을 입력하세요 (원): ");
         int paid = scanner.nextInt();
